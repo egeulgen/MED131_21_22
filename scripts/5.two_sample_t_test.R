8e7ac5e663005ed2486b0db7f46b2080e4f6c727
egeulgen <egeulgen@gmail.com>
2021-11-02 04:07:06 +0300

    add anova example

--- scripts/5.two_sample_t_test.R
+++ scripts/5.two_sample_t_test.R
@@ -40,3 +40,15 @@ sd(genderweight$weight[genderweight$group == "F"])
 ## is the mean weight of men significantly different than the mean weight of women?
 t.test(genderweight$weight~genderweight$group)
 t.test(weight~group, data = genderweight)
+
+# ANOVA -------------------------------------------------------------------
+tumor_size_df <- data.frame(size = c(3, 4, 5, 6, 4, 5, 6, 7, 7, 8, 9, 10),
+                            group = c("A","A","A","A","B","B","B","B","C","C","C","C"))
+tumor_size_df
+
+boxplot(tumor_size_df$size~tumor_size_df$group, xlab = "Group", ylab = "Tumor Size")
+
+res_aov <- aov(size ~ group, data = tumor_size_df)
+summary(res_aov)
+
+TukeyHSD(res_aov)
